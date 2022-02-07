165977a74494563e3c9f693f4f9df302308c4565
chihyeonWON <heungeob1003@gamil.com>
2022-02-07 17:06:39 +0900

    프로그램을 껐을 때 모두 종료되도록 설정

--- src/Frame.java
+++ src/Frame.java
@@ -9,5 +9,6 @@ public class Frame {
 		frame.setVisible(true);
 		frame.setPreferredSize(new Dimension(840, 840/12*9));
 		frame.setSize(840, 840/12*9); //프로그램 기본사이즈 설정
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램을 껐을 때 종료 되도록 설정
 	}
 }
