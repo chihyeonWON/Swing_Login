1a4567a4269b3402e91be957addaa3f85978ff52
chihyeonWON <heungeob1003@gamil.com>
2022-02-07 17:13:45 +0900

    panel 생성

--- src/Frame.java
+++ src/Frame.java
@@ -1,11 +1,12 @@
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		
+		JPanel panel = new JPanel();
 		
 		frame.setResizable(false);
 		frame.setVisible(true);
