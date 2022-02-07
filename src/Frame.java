498dbb37bc16f24e69dbd81b572063474ce95090
chihyeonWON <heungeob1003@gamil.com>
2022-02-07 17:21:49 +0900

    주석 설명 추가

--- src/Frame.java
+++ src/Frame.java
@@ -8,8 +8,12 @@ public class Frame {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		
-		frame.setResizable(false);
-		frame.setVisible(true);
+		frame.add(panel); // 프레임에 패널을 넣어줌
+		
+		
+		
+		frame.setResizable(false); // 프로그램 사이즈 변경 금지
+		frame.setVisible(true); // 창을 보이게 해줌
 		frame.setPreferredSize(new Dimension(840, 840/12*9));
 		frame.setSize(840, 840/12*9); //프로그램 기본사이즈 설정
 		frame.setLocationRelativeTo(null); // 프로그램 기본 위치 해제
