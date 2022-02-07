02e265461096be4a4c4160c628b0124e5f8abfb0
chihyeonWON <heungeob1003@gamil.com>
2022-02-07 17:12:07 +0900

    창 크기 조절 방지

--- src/Frame.java
+++ src/Frame.java
@@ -6,6 +6,8 @@ public class Frame {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		
+		
+		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setPreferredSize(new Dimension(840, 840/12*9));
 		frame.setSize(840, 840/12*9); //프로그램 기본사이즈 설정
