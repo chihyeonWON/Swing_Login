5bb86d7bc6e71630712212639fa43bd3144951bf
chihyeonWON <heungeob1003@gamil.com>
2022-02-08 15:22:44 +0900

    레이블 생성

--- src/Frame.java
+++ src/Frame.java
@@ -1,17 +1,18 @@
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Frame {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
+		JLabel label = new JLabel("Some text");
+		
 		
 		frame.add(panel); // 프레임에 패널을 넣어줌
-		
-		
-		
+
 		frame.setResizable(false); // 프로그램 사이즈 변경 금지
 		frame.setVisible(true); // 창을 보이게 해줌
 		frame.setPreferredSize(new Dimension(840, 840/12*9));
