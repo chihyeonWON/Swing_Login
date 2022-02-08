db74bfbdb8933e4fc3969774f84416b5ff8a8aaf
chihyeonWON <heungeob1003@gamil.com>
2022-02-08 15:36:19 +0900

    텍스트 에이리어 생성

--- src/Frame.java
+++ src/Frame.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Frame {
 	public static void main(String[] args) {
@@ -12,11 +13,13 @@ public class Frame {
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Some text");
 		JButton btn1 = new JButton("Click me");
+		JTextArea textArea = new JTextArea();
 		
 		panel.setLayout(new BorderLayout());
 		
 		panel.add(label, BorderLayout.NORTH);
 		panel.add(btn1, BorderLayout.WEST);
+		panel.add(textArea, BorderLayout.CENTER);
 		
 		frame.add(panel); // 프레임에 패널을 넣어줌
 
