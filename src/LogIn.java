edfdf1007315b0fe7acdea1a65f822a3bc599f61
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-09 16:48:52 +0900

    창 설정

--- src/LogIn.java
+++ src/LogIn.java
@@ -2,7 +2,11 @@ import javax.swing.JFrame;
 
 public class LogIn extends JFrame {
 	public LogIn() {
-		
+		setVisible(true); // 화면에 표시
+		setSize(600,400); // 사이즈변경
+		setLocationRelativeTo(null);
+		setResizable(false); // 사이즈변경금지
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
