de98ef402c37317fa7928ed5a219f6722449bcac
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-09 16:56:59 +0900

    아이디 라벨, 패널, 텍스트 필드 생성

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,7 +1,19 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class LogIn extends JFrame {
 	public LogIn() {
+		JPanel panel = new JPanel(); // ID,PW 패널
+		JLabel label = new JLabel("ID : "); // ID 라벨
+		JTextField txtID = new JTextField(10); //ID 입력창
+		
+		panel.add(label);
+		panel.add(txtID);
+		
+		add(panel);
+		
 		setVisible(true); // 화면에 표시
 		setSize(600,400); // 사이즈변경
 		setLocationRelativeTo(null);
