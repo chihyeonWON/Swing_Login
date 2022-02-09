fffc8821982a11445f39b98efa461d8fa5a903c2
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-09 16:44:24 +0900

    JFrame을 상속받는 LogIn 클래스를 생성

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,4 +1,12 @@
+import javax.swing.JFrame;
 
-public class LogIn {
-
+public class LogIn extends JFrame {
+	public LogIn() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new LogIn();
+	}
+	
 }
